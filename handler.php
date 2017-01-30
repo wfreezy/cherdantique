14a181a6c9ae3cce0bed280cc00dda7fd8600e49
wfreezy <lilwfreezy@gmail.com>
2017-01-30 20:24:58 +0300

    Create handler.php

---
+++ handler.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_REQUEST['ok'])) {
+ 
+    $string = '<p>'.$_REQUEST['username'].'<br>'.$_REQUEST['msg'];
+    file_put_contents('1.txt', $string, FILE_APPEND);
+    header("Location: /test.php");
+    exit();
+}
+include ('1.txt');
+?>
