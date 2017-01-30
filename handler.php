e763a0d6846938836141e0a9a519f925e0df6016
wfreezy <lilwfreezy@gmail.com>
2017-01-30 20:26:08 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,7 +3,7 @@ if (isset($_REQUEST['ok'])) {
  
     $string = '<p>'.$_REQUEST['username'].'<br>'.$_REQUEST['msg'];
     file_put_contents('1.txt', $string, FILE_APPEND);
-    header("Location: /test.php");
+    header("Location: /отправкасмс.php");
     exit();
 }
 include ('1.txt');
