d75a178e042d563c4fc7c047295ad5ac3109d6c4
wfreezy <lilwfreezy@gmail.com>
2017-02-01 23:22:43 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,9 +1,17 @@
 <?php
-
-$message = "Line 1\r\nLine 2\r\nLine 3";
-
-$message = wordwrap($message, 70, "\r\n");
-
-
-mail('lilwfreezy@gmail.com', 'My Subject', $message);
-?>
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
