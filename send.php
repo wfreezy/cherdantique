b7459895a14b8dfdd36c0b0a87aa81d8b4d3014f
wfreezy <lilwfreezy@gmail.com>
2017-02-02 17:22:44 +0300

    Update send.php

--- send.php
+++ send.php
@@ -10,7 +10,7 @@ $email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("lilwfreezy@gmail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+if (mail("lilwfreezy@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: lolo4nik@gmail.com \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
