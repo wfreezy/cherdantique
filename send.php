caa8464e598d20db324d307b6665080851da34fc
wfreezy <lilwfreezy@gmail.com>
2017-02-02 17:27:37 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,17 +1,3 @@
-<?php
-$fio = $_POST['fio'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("lilwfreezy@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: lolo4nik@gmail.com \r\n"))
- {     echo "сообщение успешно отправлено"; 
-} else { 
-    echo "при отправке сообщения возникли ошибки";
-}?>
+<?php 
+mail("lilwfreezyl@egmail.com", "My Subject", "Line 1\nLine 2\nLine 3"); 
+?>
