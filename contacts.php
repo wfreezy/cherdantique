1b08e2106228ae1d024f829ac59a7caca4dad19b
wfreezy <lilwfreezy@gmail.com>
2017-02-05 19:06:56 +0300

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -43,7 +43,7 @@
 
 <?
 if (isset ($_POST['messageFF'])) {
-  mail ("свой@yandex.ru",
+  mail ("lilwfreezy@gmail.com",
         "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
         "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nСообщение: ".$_POST['messageFF']);
   echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
